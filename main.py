08f2fbb6c0d4f41e185cd8ab89b3e5249247faf2
RD Community <85223036+oshara145@users.noreply.github.com>
2021-11-07 17:40:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import telebot
 
-bot = telebot.Telebot('api key here')
+bot = telebot.Telebot('2144282821:AAEBYkPU6dLOFrbO3Kf8FDmIOzqX7ircTkI')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message,'Hello! Im' Uvindu Bro Chat Bot')
+  bot.reply_to(message,'Hello! Im Oshara How Are You')
 
 @bot.message_handler(commands=['how])
 def send_message(message):
