00f28ffeb05880f56eb337ec9b853f020f7e6a03
RD Community <85223036+oshara145@users.noreply.github.com>
2021-12-17 18:01:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 import telebot
 
-bot = telebot.Telebot('2144282821:AAEBYkPU6dLOFrbO3Kf8FDmIOzqX7ircTkI')
+bot = telebot.Telebot('BOT_TOKEN')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message,'Hello! Im Oshara How Are You')
+  bot.reply_to(message,'Hello! Im {YOUR_NAME} How Are You')
 
 @bot.message_handler(commands=['how])
 def send_message(message):
-  bot.send_message(message, 'fuck you')
+  bot.send_message(message, 'Im Fine 🙂')
   
   bot.polling()
